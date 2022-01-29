38ee605379ee26888388cce93bad781d52c92874
Colderry <colderryz@gmail.com>
2022-01-29 14:54:46 +0600

    Redirect to code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import Database from "#utils/manage-database";
 import api from "./api";
 
 const router = Router();
 
 router.use("/api", api);
 
+router.get("/:code", async (req, res) => {
+    if (!req.params.code) {
+        return;
+    };
+
+    const data = await Database.getURL(req.params.code);
+
+    if (!data) {
+        return;
+    } else {
+        res.redirect(data.Redirect);
+    };
+});
 
 export default router;
