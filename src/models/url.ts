3c28c804bc6dac1779bb652ed8c72298ae2e48a7
Colderry <colderryz@gmail.com>
2022-01-29 14:45:56 +0600

    Model(url): The schema for the urls

---
+++ src/models/url.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema, model, Document } from "mongoose";
+
+export interface URLInterface extends Document {
+    _id: string;
+    Redirect: string;
+    Code: string;
+}
+
+const URL = new Schema({
+    Redirect: {
+        type: String,
+        required: true
+    },
+    Code: {
+        type: String,
+        required: true
+    }
+}, {
+    versionKey: false,
+    timestamps: true
+});
+
+export default mongoose.models.urls || model<URLInterface>("urls", URL);
