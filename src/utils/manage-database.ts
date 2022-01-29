28c4776b38840ea8ea85f6be57fea77d0cfc7443
Colderry <colderryz@gmail.com>
2022-01-29 14:55:18 +0600

    Modify: (getURL)

--- src/utils/manage-database.ts
+++ src/utils/manage-database.ts
@@ -2,61 +2,61 @@ import SchemaURL from "#models/url";
 import createCode from "#utils/create-code";
 
 class Database {
-  constructor() {}
+    constructor() { }
 
-  public async getURL(code: string) {
-    return new Promise(async (resolve, reject) => {
-      const url = await SchemaURL.findOne({ _id: String(code) });
+    public async getURL(Code: string): Promise<any> {
+        return new Promise(async (resolve, reject) => {
+            const url = await SchemaURL.findOne({ Code });
 
-      if (!url) {
-        return resolve(null);
-      } else {
-        return resolve(url);
-      }
-    });
-  }
+            if (!url) {
+                return resolve(null);
+            } else {
+                return resolve(url);
+            };
+        });
+    };
 
-  public async createURL(Redirect: string) {
-    return new Promise(async (resolve, reject) => {
-      const Code = createCode();
+    public async createURL(Redirect: string) {
+        return new Promise(async (resolve, reject) => {
+            const Code = createCode();
 
-      const url = new SchemaURL({ Redirect, Code });
-      await url.save();
+            const url = new SchemaURL({ Redirect, Code });
+            await url.save();
 
-      return resolve(Code);
-    });
-  };
+            return resolve(Code);
+        });
+    };
 
-  public async editURL(code: string, data: editURLData): Promise<boolean> {
-    return new Promise(async (resolve, reject) => {
-      const body = {} as { code: string; redirect: string };
+    public async editURL(code: string, data: editURLData): Promise<boolean> {
+        return new Promise(async (resolve, reject) => {
+            const body = {} as { code: string; redirect: string };
 
-      if (code) {
-        body["code"] = code;
-      }
+            if (code) {
+                body["code"] = code;
+            }
 
-      if (data.redirect) {
-        body["redirect"] = data.redirect;
-      }
+            if (data.redirect) {
+                body["redirect"] = data.redirect;
+            }
 
-      const url = await SchemaURL.findOne({ _id: String(code) });
+            const url = await SchemaURL.findOne({ _id: String(code) });
 
-      if (!url) {
-        return resolve(false);
-      } else {
-        if (body["code"]) url["_id"] = body["code"];
-        if (body["redirect"]) url["Redirect"] = body["redirect"];
+            if (!url) {
+                return resolve(false);
+            } else {
+                if (body["code"]) url["_id"] = body["code"];
+                if (body["redirect"]) url["Redirect"] = body["redirect"];
 
-        url.save();
-        return resolve(true);
-      };
-    });
-  };
+                url.save();
+                return resolve(true);
+            };
+        });
+    };
 };
 
 export interface editURLData {
-  code?: string;
-  redirect: string;
+    code?: string;
+    redirect: string;
 };
 
 export default new Database();
