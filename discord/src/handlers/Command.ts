6e0e4fdc19db61bdc29c2eca40436d6fca804d29
Colderry <colderryz@gmail.com>
2022-01-30 11:43:20 +0600

    Using glob instead of fs

--- discord/src/handlers/Command.ts
+++ discord/src/handlers/Command.ts
@@ -1,12 +1,13 @@
 import { Discord } from "..";
-import fs from "fs";
+import glob from "glob";
+import path from "path";
 
 export default function (client: Discord) {
 
-    const files = fs.readdirSync(process.cwd() + "/build/commands/").filter(x => x.endsWith(".js"));
+    const files = glob.sync("./build/commands/**/**/*.js");
 
     for (let i = 0; i < files.length; i++) {
-        const file = require(`../commands/${files[i]}`).default;
+        const file = require(path.resolve(files[i])).default;
 
         if (file.name) {
             client.cache.set(`commands-${file.name}`, file);
